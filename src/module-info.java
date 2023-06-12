c2fb03ca50b78f0cb246ffabd167b6ce37d39501
Md Morshad Alam <riyadhswe@gmail.com>
2023-06-12 10:30:56 +0600

    init

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author riyad
+ *
+ */
+module SQAJava {
+}
---
+++ src/root/App.java
@@ -0,0 +1,12 @@
+package root;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Riyadh");
+
+	}
+
+}
+ 
